1d3e322c00888dba4664f24572c3eca998869b1f
zolroyd <zolroyd@gmail.com>
2020-01-31 17:59:28 -0700

    more factoring and adding in nav menu. next is trying out redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
     var car = {
@@ -16,17 +20,18 @@ function App() {
     };
     return (
         <div className="App">
-            <CarDisplay x={2} cars={[car, car2]} y={1}></CarDisplay>
+            <NavHeader></NavHeader>
+            <CarDisplay cars={[car, car2]} x='2' y='1'></CarDisplay>
         </div>
     );
 }
 
 function CarCard({car}) {
-    return <div className="card col-md-2">
+    return <div className="card col-md-2" style={{margin: 5}}>
         <img className="card-img-top" src="../images/car1.jpeg" alt="Car cap"></img>
         <div className="card-body" style={{paddingLeft: 0}}>
             <div className="card-title" style={{marginBottom: 0}}><b>Details</b></div>
-            <CarMileage mileage={car.mileage}></CarMileage>
+            <CarMileage mileage={car.mileage} key="mileage"></CarMileage>
             <CarColor color={car.color}></CarColor>
             <CarOptions options={car.options}></CarOptions>
             <a href="#test" className="col-md-12 btn btn-primary btn-lg text-center"
@@ -68,23 +73,57 @@ function CarOptions({options}) {
         </ul> : '';
 }
 
-function CarDisplay(props) {
-    var cars = props.cars;
-    var x = props.x;
-    var y = props.y;
-    var carDisplay;
+class CarDisplay extends React.Component {
+    render() {
+        var cars = this.props.cars;
+        var x = this.props.x;
+        var y = this.props.y;
+        var carDisplay;
+        var returnVal;
 
-    for (let i = 0; i < x; i++) {
-        for (let i2 = 0; i2 < y; i2++) {
-            let index = i + i2;
-            let car = cars[i + i2];
+        for (let i = 0; i < y; i++) {
+            for (let i2 = 0; i2 < x; i2++) {
+                let index = i + i2;
+                let car = cars[i + i2];
 
-            if (car !== undefined) {
-                carDisplay = [carDisplay, <CarCard key={car.key} car={car}></CarCard>];
+                if (car !== undefined) {
+                    carDisplay = [carDisplay, <CarCard key={car.key} car={car}></CarCard>];
+                }
             }
+            returnVal = <div className="col-md-12 col-sm-12 col-xs-12"><div className="row"> {carDisplay} </div> </div>;
+            carDisplay = [];
         }
+        return returnVal;
     }
-    return carDisplay;
+}
+
+function NavHeader(props) {
+    return <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">
+            <img
+                alt=""
+                src="images/logo.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+            />{' '}
+            Your website
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider/>
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>;
 }
 
 export default App;
